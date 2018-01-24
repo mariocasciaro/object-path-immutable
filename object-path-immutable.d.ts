7d4bc87164d113cee90c7bc5fe418077d3f0c781
Maxim Syabro <maxim@syabro.com>
2018-01-24 14:38:38 +0300

    Add typescript definitions

---
+++ object-path-immutable.d.ts
@@ -0,0 +1,22 @@
+type Path = string | string[]
+
+interface WrappedObject<T> {
+    set(path?: Path, value?: any): WrappedObject<T>
+    push(path?: Path, value?: any): WrappedObject<T>
+    del(path?: Path): WrappedObject<T>
+    assign(path?: Path, source?: T): WrappedObject<T>
+    value(): T
+}
+
+interface ObjectPathImmutable {
+    <T>(obj: T): WrappedObject<T>
+    set<T = object>(src: T, path?: Path, value?: any): T
+    push<T = object>(src: T, path?: Path, value?: any): T
+    del<T = object>(src: T, path?: Path): T
+    assign<T = object>(src: T, path?: Path, source?: T): T
+}
+
+declare module 'object-path-immutable' {
+    const immutable: ObjectPathImmutable;
+    export default immutable
+}
