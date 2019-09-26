14825b136703c715257bd56b7d1d5060db3fce77
Mario Casciaro <mariocasciaro@gmail.com>
2019-09-26 16:35:34 +0100

    (#35) Typescript definitions incorrectly list default export

--- object-path-immutable.d.ts
+++ object-path-immutable.d.ts
@@ -11,18 +11,13 @@ interface WrappedObject<T> {
     value(): T
 }
 
-interface ObjectPathImmutable {
-    wrap<T>(obj: T): WrappedObject<T>
-    set<T = object>(src: T, path?: Path, value?: any): T
-    push<T = object>(src: T, path?: Path, value?: any): T
-    del<T = object>(src: T, path?: Path): T
-    assign<T = object>(src: T, path?: Path, source?: T): T
-    merge<T = object>(src: T, path?: Path, source?: any): T
-    update<T = object>(src: T, path?: Path, updater?: (formerValue: any) => any): WrappedObject<T>
-    insert<T = object>(src: T, path?: Path, value?: any, index?: number): T
-}
-
 declare module 'object-path-immutable' {
-    const immutable: ObjectPathImmutable;
-    export default immutable
+    export function wrap<T>(obj: T): WrappedObject<T>
+    export function set<T = object>(src: T, path?: Path, value?: any): T
+    export function push<T = object>(src: T, path?: Path, value?: any): T
+    export function del<T = object>(src: T, path?: Path): T
+    export function assign<T = object>(src: T, path?: Path, source?: T): T
+    export function merge<T = object>(src: T, path?: Path, source?: any): T
+    export function update<T = object>(src: T, path?: Path, updater?: (formerValue: any) => any): WrappedObject<T>
+    export function insert<T = object>(src: T, path?: Path, value?: any, index?: number): T
 }
